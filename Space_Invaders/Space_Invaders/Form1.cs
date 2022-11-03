55e044cd3ff0eb13d84ad6e3c3b8cc0ed6eb574f
Jean Carlos Lerma <yancalerma@gmail.com>
2022-11-03 14:35:20 -0500

    2022-11-02 1434 Se creó la clase GamePiece de donde van a salir las piezas para el juego En ella se asigna la vida, la imagen, la posición, tamaño

--- Space_Invaders/Space_Invaders/Form1.cs
+++ Space_Invaders/Space_Invaders/Form1.cs
@@ -1,3 +1,6 @@
+using Microsoft.VisualBasic;
+using System.Reflection;
+
 namespace Space_Invaders
 {
     public partial class Form1 : Form
@@ -5,6 +8,22 @@ namespace Space_Invaders
         public Form1()
         {
             InitializeComponent();
+            pictureBoxCreate();
         }
+
+        public void pictureBoxCreate()
+        {
+            string basePath = Environment.CurrentDirectory;
+            string relativePath = "../../../assets/img/nave.gif";
+            string finalPath = Path.GetFullPath(relativePath, basePath);
+            PictureBox pictureImage = new PictureBox();
+            pictureImage.Image = Image.FromFile(finalPath);
+            pictureImage.Location = new System.Drawing.Point(100, 100);
+            pictureImage.Size = new System.Drawing.Size(100, 100);
+            Controls.Add(pictureImage);
+            System.Diagnostics.Debug.WriteLine(finalPath);
+        }
+        
+        
     }
 }
---
+++ Space_Invaders/Space_Invaders/GamePiece.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Space_Invaders
+{
+    internal class GamePiece
+    {
+        public int id { get; set; }
+        public string image { get; set; }
+        public int[] location { get; set; }
+        public int life { get; set; }
+        public int[] size { get; set; }
+
+        public GamePiece(int id, string image, int[] location, int life, int[] size)
+        {
+            this.id = id;
+            this.image = image;
+            this.location = location;
+            this.life = life;
+            this.size = size;
+        }
+    }
+}
