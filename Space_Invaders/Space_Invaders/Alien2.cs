2733ff5fe7a946ecbc55bcb68f60867ca79ac1ed
Jean-Carlos-L <yancarojas2003@gmail.com>
2022-11-05 12:09:21 -0500

    Jean Carlos Lerma Se creo clases y metodos para agregar al form

---
+++ Space_Invaders/Space_Invaders/Alien2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Space_Invaders
+{
+    internal class Alien2 : GamePiece
+    {
+        public Alien2(int id, string image, int[] location, int life, int[] size) : base(id, image, location, life, size)
+        {
+        }
+    }
+}
