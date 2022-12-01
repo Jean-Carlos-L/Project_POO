54798db69f590bf74fac5bb8905766e23790fe0f
Jean-Carlos-L <yancarojas2003@gmail.com>
2022-12-01 10:12:15 -0500

    Backup

--- Space_Invaders/Space_Invaders/Form1.Designer.cs
+++ Space_Invaders/Space_Invaders/Form1.Designer.cs
@@ -28,15 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.vida3 = new System.Windows.Forms.PictureBox();
+            this.vida1 = new System.Windows.Forms.PictureBox();
+            this.vida2 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.vida3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.vida1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.vida2)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -61,35 +63,41 @@
             this.label2.TabIndex = 1;
             this.label2.Text = "000";
             // 
-            // pictureBox1
+            // vida3
             // 
-            this.pictureBox1.BackColor = System.Drawing.SystemColors.WindowText;
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(1143, 0);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(35, 35);
-            this.pictureBox1.TabIndex = 2;
-            this.pictureBox1.TabStop = false;
+            this.vida3.BackColor = System.Drawing.SystemColors.WindowText;
+            this.vida3.Image = ((System.Drawing.Image)(resources.GetObject("vida3.Image")));
+            this.vida3.Location = new System.Drawing.Point(1143, 0);
+            this.vida3.Name = "vida3";
+            this.vida3.Size = new System.Drawing.Size(35, 35);
+            this.vida3.TabIndex = 2;
+            this.vida3.TabStop = false;
             // 
-            // pictureBox2
+            // vida1
             // 
-            this.pictureBox2.BackColor = System.Drawing.SystemColors.WindowText;
-            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
-            this.pictureBox2.Location = new System.Drawing.Point(1059, 0);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(35, 35);
-            this.pictureBox2.TabIndex = 3;
-            this.pictureBox2.TabStop = false;
+            this.vida1.BackColor = System.Drawing.SystemColors.WindowText;
+            this.vida1.Image = ((System.Drawing.Image)(resources.GetObject("vida1.Image")));
+            this.vida1.Location = new System.Drawing.Point(1059, 0);
+            this.vida1.Name = "vida1";
+            this.vida1.Size = new System.Drawing.Size(35, 35);
+            this.vida1.TabIndex = 3;
+            this.vida1.TabStop = false;
             // 
-            // pictureBox3
+            // vida2
             // 
-            this.pictureBox3.BackColor = System.Drawing.SystemColors.WindowText;
-            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
-            this.pictureBox3.Location = new System.Drawing.Point(1101, 0);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(35, 35);
-            this.pictureBox3.TabIndex = 4;
-            this.pictureBox3.TabStop = false;
+            this.vida2.BackColor = System.Drawing.SystemColors.WindowText;
+            this.vida2.Image = ((System.Drawing.Image)(resources.GetObject("vida2.Image")));
+            this.vida2.Location = new System.Drawing.Point(1101, 0);
+            this.vida2.Name = "vida2";
+            this.vida2.Size = new System.Drawing.Size(35, 35);
+            this.vida2.TabIndex = 4;
+            this.vida2.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
@@ -97,9 +105,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.ClientSize = new System.Drawing.Size(1180, 657);
-            this.Controls.Add(this.pictureBox3);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.vida2);
+            this.Controls.Add(this.vida1);
+            this.Controls.Add(this.vida3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
@@ -111,9 +119,9 @@
             this.Text = "Space Invaders";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.vida3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.vida1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.vida2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -123,8 +131,9 @@
 
         private Label label1;
         private Label label2;
-        private PictureBox pictureBox1;
-        private PictureBox pictureBox2;
-        private PictureBox pictureBox3;
+        private PictureBox vida3;
+        private PictureBox vida1;
+        private PictureBox vida2;
+        private System.Windows.Forms.Timer timer1;
     }
 }
