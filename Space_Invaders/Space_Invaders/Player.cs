8c9ac48270dc27e0f61519eb555064030f951780
camilogsaenz <117381403+camilogsaenz@users.noreply.github.com>
2022-12-04 15:49:21 -0500

    Update Player.cs

--- Space_Invaders/Space_Invaders/Player.cs
+++ Space_Invaders/Space_Invaders/Player.cs
@@ -8,6 +8,7 @@ namespace Space_Invaders
 {
     internal class Player
     {
+        //Clase Player para registrar el nombre y el puntaje del jugador.
         public string name { get; set; }
         public string score { get; set; }
 
